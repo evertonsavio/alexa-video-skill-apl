4ab124ad5d32275ec22d6478b59c64459e09ccdb
Havyx <everluca@outlook.com>
2021-07-19 16:29:07 -0300

    :sparkles: lambda video skill

---
+++ lambda/index.js
@@ -0,0 +1,140 @@
+const Alexa = require('ask-sdk-core');
+
+const PlayVideoIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
+      || (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'PlayVideoIntent');
+  },
+  handle(handlerInput) {
+      
+    const speechOutput = "OK, vamos lá...";
+    
+    if(supportsAPL(handlerInput)) {
+              return handlerInput.responseBuilder
+        .speak(speechOutput)
+        .addDirective({
+          type: 'Alexa.Presentation.APL.RenderDocument',
+          document: require('./apl/layouts/videoPlayer.json'),
+          token: "VideoPlayerToken"
+        })
+        .addDirective({
+          type: "Alexa.Presentation.APL.ExecuteCommands",
+          token: "VideoPlayerToken",
+          commands: [
+            {
+              type: "ControlMedia",
+              componentId: "myVideoPlayer",
+              command: "play"
+            }
+          ]
+        })
+        .getResponse();
+            
+    }
+    else {
+      return handlerInput.responseBuilder
+        .speak("Ocorreu um erro, verifique se esse dispositivo suporta reproduzir vídeos.")
+        .getResponse();
+    }
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'Essa skill reproduz um video.';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const AboutIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AboutIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'Essa é uma skill que reproduz um video.';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Tchauzinho!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Não entendi o comando. Poderia repetir por gentileza.')
+      .reprompt('Desculpe-me, não entendi o comando. Poderia dizer novamente.')
+      .getResponse();
+  },
+};
+
+function supportsAPL(handlerInput) {
+    const supportedInterfaces = handlerInput.requestEnvelope.context.System.device.supportedInterfaces;
+    const aplInterface = supportedInterfaces['Alexa.Presentation.APL'];
+    return aplInterface !== null && aplInterface !== undefined;
+  }
+
+function supportsDisplay(handlerInput) {
+  const hasDisplay =
+    handlerInput.requestEnvelope.context &&
+    handlerInput.requestEnvelope.context.System &&
+    handlerInput.requestEnvelope.context.System.device &&
+    handlerInput.requestEnvelope.context.System.device.supportedInterfaces &&
+    handlerInput.requestEnvelope.context.System.device.supportedInterfaces.Display;
+  return hasDisplay;
+}
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    PlayVideoIntentHandler,
+    AboutIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
